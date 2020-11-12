ee392c01f7acbe6a7900bd37012f3a1e5964ca8c
aenis <aenis@live.fr>
2020-11-12 12:03:23 +0100

    commit test 1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MRK_PYTHON',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='AENIS ROSTANE',
+    author_email='',
+    description=''
+)
